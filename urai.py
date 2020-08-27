c1d759c385b98fe800a7f562bfba161d7174df62
JonathanAditia <70124595+JonathanAditia@users.noreply.github.com>
2020-08-27 12:59:30 +0700

    Create urai.py

---
+++ urai.py
@@ -0,0 +1,7 @@
+def urai(kata): #func urai
+    liststr = '' #var string kosong
+    for i in kata: #loop per huruf di kata
+        liststr += i #liststr + huruf
+        print(liststr, end='') #print liststr, dibuat menyamping
+    
+urai('Code')
